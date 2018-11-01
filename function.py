ff8e19a1521f773fc9a2af8aa7209dc7046f4018
unknown <luckyhiman@gmail.com>
2018-11-01 17:58:13 +0530

    function

---
+++ function.py
@@ -0,0 +1,20 @@
+def my_function():
+    print("Hello, this is my first function")
+
+my_function()  # calling function
+
+def my_function(fname):
+    print(fname + "Smart")
+
+my_function("Himanshu ")
+my_function("Pandit ")
+my_function("Rajesh ")  # Information can be passed to functions as parameter
+
+def my_function(country = "Norway"):
+    print("I am from " + country)
+
+my_function("India")
+my_function("England")
+my_function()
+my_function("Pakistan")
+
